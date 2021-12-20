7884e3b27afddff0e689f4b996f92a845b5e9273
Ali <Ali@ALI-X542>
2021-12-20 17:42:11 +0330

    Add project files.

---
+++ ASP5Bootstrap/Pages/Models/HeaderLinkModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IVC_WebApp.Pages.Models
+{
+    public class HeaderLinkModel
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string? IconName { get; set; }
+    }
+}
