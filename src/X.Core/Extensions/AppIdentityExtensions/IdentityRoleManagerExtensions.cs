fa94ce2d7ea413186e4bc352ecb3a382ce460655
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-06-05 13:19:36 +0430

    Core: UserManager And RoleManager Extensions For Add

---
+++ src/X.Core/Extensions/AppIdentityExtensions/IdentityRoleManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace X.Core.Extensions.AppIdentityExtensions
+{
+	public static class IdentityRoleManagerExtensions
+	{
+		public static async Task<IdentityResult> AddRole(this RoleManager<IdentityRole> @this, string roleName)
+		{
+			return await @this.CreateAsync(new IdentityRole(roleName));
+		}
+	}
+}
